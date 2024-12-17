ab30b5d68e8373b441b87cdc7f7482bf78874b3c
1mache <miktor4925@gmail.com>
2024-12-17 14:44:45 +0200

    Organized barrelManager a bit

--- DonkeyKong/BarrelManager.cpp
+++ DonkeyKong/BarrelManager.cpp
@@ -1,34 +1,18 @@
-#include "BarrelManager.h"
-
-void BarrelManager::spawnBarrel()
-{   
-
-    //                          barrel constructor 
-    barrelsVector.emplace_back(gameBoard, gameBoard->BARREL, spawnerPosition + DIRECTIONS[curBarrelDir], DIRECTIONS[curBarrelDir]);
-}
-
-void BarrelManager::destroyBarrel(int index)
-{
-    barrelsVector.erase(barrelsVector.begin() + index);
-}
+#include "BarrelManager.h"s
 
 void BarrelManager::moveAllBarrels()
 {
-    //move barrels every other frame so that they are slower than mario 
-    /*if(frameCounter % 2 == 0)*/
+    for (size_t i = 0; i < barrelsVector.size(); i++)
     {
-        for (size_t i = 0; i < barrelsVector.size(); i++)
+        if (barrelsVector[i].exploded())
         {
-            if (barrelsVector[i].exploded())
-            {
-                barrelsVector[i].update();
-                destroyBarrel(i);
-            }
+            barrelsVector[i].update();
+            deleteBarrel(i);
+        }
             
-            if(!barrelsVector.empty())
-            {
-                barrelsVector[i].update();
-            }
+        if(!barrelsVector.empty())
+        {
+            barrelsVector[i].update();
         }
     }
 }
--- DonkeyKong/BarrelManager.h
+++ DonkeyKong/BarrelManager.h
@@ -16,8 +16,8 @@ class BarrelManager
     Point spawnerPosition;
     int frameCounter = 0;
     bool spawnToRight = true;
-    
-    enum SpawnDirection {LEFT, RIGHT};
+
+    enum SpawnDirection { LEFT, RIGHT };
     static constexpr Point DIRECTIONS[] = { {-1, 0}, {1, 0} };
 
     SpawnDirection curBarrelDir = SpawnDirection::RIGHT;
@@ -25,11 +25,24 @@ class BarrelManager
     Board* gameBoard = nullptr;
     std::vector<Barrel> barrelsVector;
 
-    void spawnBarrel();
-    void destroyBarrel(int index);
+    void spawnBarrel()
+    {
+        // barrel constructor, creates barrel in the vector 
+        barrelsVector.emplace_back(gameBoard, gameBoard->BARREL, spawnerPosition + DIRECTIONS[curBarrelDir], DIRECTIONS[curBarrelDir]);
+    }
+
+    void deleteBarrel(int index)
+    {
+        //deletes the barrel from the barrel vector
+        barrelsVector.erase(barrelsVector.begin() + index);
+    }
+
     void moveAllBarrels();
 
-    int getRandomDelay() { return ((rand() % (MAX_SPAWN_DELAY + 1 - MIN_SPAWN_DELAY)) + MIN_SPAWN_DELAY); };
+    int getRandomDelay() 
+    { 
+        return ((rand() % (MAX_SPAWN_DELAY + 1 - MIN_SPAWN_DELAY)) + MIN_SPAWN_DELAY); 
+    }
 
 public:
     BarrelManager(Board* _gameBoard, Point _spawnerPos) : spawnerPosition(_spawnerPos), gameBoard(_gameBoard) {}
